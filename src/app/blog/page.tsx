7096316660730e9c2edf74c3a718d0535bfb0198
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2023-11-29 16:01:34 +0800

    first blogpost

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,32 @@
+import { Post, allPosts } from "contentlayer/generated";
+import { compareDesc, format, parseISO } from "date-fns";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Blog Posts</h1>
+      <div className="flex flex-col gap-2">
+        {allPosts
+          .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+          .map((post, n) => (
+            <BlogCard post={post} key={n} />
+          ))}
+      </div>
+    </main>
+  );
+}
+
+function BlogCard({ post }: { post: Post }) {
+  return (
+    <Link href={post.url}>
+      <div className="rounded-md bg-neutral-800 transition-all hover:text-white shadow-md p-2">
+        <p className="underline underline-offset-4">{post.title}</p>
+        <time dateTime={post.date} className="text-xs">
+          {format(parseISO(post.date), "LLL d, yyyy")}
+        </time>
+        <p>{post.desc}</p>
+      </div>
+    </Link>
+  );
+}
--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -1,20 +1,39 @@
+import { Post, allPosts } from "contentlayer/generated";
+import { compareDesc, format, parseISO } from "date-fns";
+import { MoveRight } from "lucide-react";
 import Link from "next/link";
 
 export function Blog() {
   return (
     <>
       <h1>Blog</h1>
-      <div className="flex flex-col gap-2">
-        <BlogLink title="" link="" />
+      <div className="flex flex-col gap-2 mb-4">
+        {allPosts
+          .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+          .slice(0, 4)
+          .map((post, n) => (
+            <BlogLink post={post} key={n} />
+          ))}
+        <Link
+          href={"/blog"}
+          className="flex gap-2 underline underline-offset-4 hover:text-white transition-all"
+        >
+          All posts <MoveRight strokeWidth={1} />
+        </Link>
       </div>
     </>
   );
 }
 
-function BlogLink({ title, link }: { title: string; link: string }) {
+function BlogLink({ post }: { post: Post }) {
   return (
-    <Link href={link} target="_blank">
-      {title}
+    <Link href={post.url}>
+      <div className="flex justify-between gap-2 hover:text-white transition-all">
+        <p className="underline underline-offset-4">{post.title}</p>
+        <time dateTime={post.date} className="min-w-fit">
+          {format(parseISO(post.date), "LLL d, yyyy")}
+        </time>
+      </div>
     </Link>
   );
 }
