088c116787c72c27c32270f287ada2ca494d74b0
icedcoffeeee <hazim.saharuddin@gmail.com>
2024-07-12 14:54:58 +0800

    view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
 	import '../app.css';
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate(function (nav) {
+		///@ts-ignore
+		if (!document.startViewTransition) return;
+		return new Promise((res) =>
+			///@ts-ignore
+			document.startViewTransition(async function () {
+				res();
+				await nav.complete;
+			})
+		);
+	});
 </script>
 
 <nav class="mb-5 pr-5 w-full flex justify-end gap-5 md:text-2xl font-['Slackside_One',_sans-serif]">
