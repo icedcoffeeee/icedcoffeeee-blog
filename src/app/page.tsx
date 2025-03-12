485c7ce6a465a2341d0d704e949b2fbf4785f236
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-03-12 21:17:10 +0800

    hold orbital, correct attribution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
-import { WormHole } from "@/components/wormhole";
-import { getProjects, getShaders, Project } from "@/lib";
 import { ChevronDown } from "lucide-react";
-import { Orbital } from "@/components/orbital";
+//import { Orbital } from "@/components/orbital";
+import { WormHole } from "@/components/wormhole";
+//import { getShaders } from "@/lib";
+import { getProjects, type Project } from "@/lib";
 
 export default function Page() {
   const projects = getProjects();
-  const shaders = getShaders();
+  //const shaders = getShaders();
   return (
-    <main className="min-h-[200svh]">
+    <main>
       <section className="-m-4 p-4 h-[calc(100svh-15*var(--spacing))] flex flex-col justify-between items-center">
         <div className="self-start">
           <h1 className="m-0 text-4xl font-mono">Hazim Saharuddin</h1>
@@ -19,54 +20,53 @@ export default function Page() {
           There are two worlds we live in: a material world, bound by the laws
           of physics, and the world inside our mind, which is just as important.
           <br />
-          &mdash; A. Einstein
+          &mdash; A. Moore
         </blockquote>
         <ChevronDown className="absolute bottom-4 animate-bounce"></ChevronDown>
       </section>
 
-      <section className="relative -m-4 min-h-svh w-svw flex justify-center items-center">
-        <div className="relative size-30 rounded-full overflow-clip shadow z-10">
-          <Image src="/images/me.jpg" alt="me" fill></Image>
-        </div>
-        <Orbital shaders={shaders}></Orbital>
-      </section>
+      {
+        //<section className="relative -m-4 min-h-svh w-svw flex justify-center items-center">
+        //<div className="relative size-30 rounded-full overflow-clip shadow z-10">
+        //<Image src="/images/me.jpg" alt="me" fill></Image>
+        //</div>
+        //<Orbital shaders={shaders}></Orbital>
+        //</section>
+      }
 
       <h1>My Projects</h1>
-      <Projects projects={projects}></Projects>
+      <section className="my-5 grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {projects.map((p, i) => (
+          <Project key={i} project={p}></Project>
+        ))}
+      </section>
     </main>
   );
 }
 
-function Projects({ projects }: { projects: Project[] }) {
+function Project({ project }: { project: Project }) {
   return (
-    <section className="my-5 grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-      {projects.map((p, i) => (
+    <div className="p-4 bg-foreground/10 backdrop-blur rounded shadow flex gap-4 transition-all hover:scale-110 hover:z-10 hover:rotate-[1deg]">
+      <div className="w-full flex flex-col justify-between">
+        <p className="font-bold">{project.title}</p>
+        <p className="line-clamp-2">{project.desc}</p>
+      </div>
+      {!!project.img && (
         <div
-          key={i}
-          className="p-4 bg-foreground/10 backdrop-blur rounded shadow flex gap-4 transition-all hover:scale-110 hover:z-10 hover:rotate-[1deg]"
+          className="aspect-square h-full bg-contain rounded overflow-clip"
+          style={{ position: "relative" }}
         >
-          <div className="w-full flex flex-col justify-between">
-            <p className="font-bold">{p.title}</p>
-            <p className="line-clamp-2">{p.desc}</p>
-          </div>
-          {!!p.img && (
-            <div
-              className="aspect-square h-full bg-contain rounded overflow-clip"
-              style={{ position: "relative" }}
-            >
-              <Image
-                src={"/images/" + p.img}
-                alt={p.img.split(".")[0]}
-                fill
-                className={
-                  "shadow object-cover" +
-                  (p.img.split(".")[1] === "svg" && " scale-50")
-                }
-              ></Image>
-            </div>
-          )}
+          <Image
+            src={"/images/" + project.img}
+            alt={project.img.split(".")[0]}
+            fill
+            className={
+              "shadow object-cover" +
+              (project.img.split(".")[1] === "svg" && " scale-50")
+            }
+          ></Image>
         </div>
-      ))}
-    </section>
+      )}
+    </div>
   );
 }
